c53ae8e57666dc821a1e7227750e161b0615447b
ryanspivey <rlspivey@go.olemiss.edu>
2021-07-08 13:07:57 -0500

    added dotenv port functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // require server and launch it
 const server = require('./api/server');
 
-server.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
